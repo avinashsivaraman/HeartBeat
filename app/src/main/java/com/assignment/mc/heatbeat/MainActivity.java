2acd01bc1902b9e2c278f13c4cc063e0efe82c23
Avinash <avinashcegian95@gmail.com>
2018-11-30 16:02:46 -0700

    Added the Asset folder

--- app/src/main/java/com/assignment/mc/heatbeat/MainActivity.java
+++ app/src/main/java/com/assignment/mc/heatbeat/MainActivity.java
@@ -1,77 +1,130 @@
 package com.assignment.mc.heatbeat;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.Spinner;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.assignment.mc.heatbeat.model.MyBarDataSet;
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.data.BarData;
-import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.utils.ColorTemplate;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Random;
-import java.util.concurrent.TimeUnit;
-import java.util.logging.Logger;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    BarChart chart = null;
-    EditText textUserName = null;
-    Button scan = null;
-    Context context = MainActivity.this;
+    private BarChart chart = null;
+    private EditText textUserName = null;
+    private Button scan = null;
+    private Context context = MainActivity.this;
+    private ImageView mImageView;
+    private Boolean isBradycardia = Boolean.FALSE;
+    private TextView textViewDisplay = null;
 
-    Float[] demoData =  {81.600f, 86.880f, 85.440f, 91.680f, 90.240f, 87.840f, 83.520f, 83.040f, 84.480f, 81.600f, 79.200f, 78.240f, 79.200f, 96f, 96.480f, 81.120f, 81.600f, 80.640f, 89.280f, 88.800f, 80.640f, 84.480f, 86.400f, 83.520f, 78.720f, 80.160f, 85.920f, 96.960f, 84.960f, 148.32f, 69.120f, 71.520f, 52.320f, 12.480f, 78.240f, 73.440f, 72.480f};
+    List<Float> demoData = new ArrayList<>();
+    Map<String, String> fileName = new HashMap<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fileName.put("Avinash", "data1");
+        fileName.put("Jaden", "data2");
+        fileName.put("Melvin", "data3");
+        fileName.put("Rishabh", "data4");
         chart = (BarChart) findViewById(R.id.barchart);
         textUserName = (EditText) findViewById(R.id.edit_user);
         Button go = (Button) findViewById(R.id.button_go);
         scan = (Button) findViewById(R.id.button_scan);
-        scan.setVisibility(View.INVISIBLE);
-        chart.setVisibility(View.INVISIBLE);
+        mImageView = (ImageView) findViewById(R.id.image1);
+        textViewDisplay = (TextView) findViewById(R.id.text_result);
+
+//       Make all the charts Invisible
+        scan.setVisibility(View.GONE);
+        chart.setVisibility(View.GONE);
+        mImageView.setVisibility(View.GONE);
+        textViewDisplay.setVisibility(View.GONE);
+//
         go.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Code here executes on main thread after user presses button
+                chart.setVisibility(View.GONE);
+                textViewDisplay.setVisibility(View.GONE);
+                showImage(fileName.get(textUserName.getText().toString()));
                 scan.setVisibility(View.VISIBLE);
-                setChartData(textUserName.getText().toString());
             }
         });
 
         scan.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Code here executes on main thread after user presses button
-                chart.setVisibility(View.INVISIBLE);
-                Toast.makeText(getApplicationContext(), "Scanning the Heart Beat", Toast.LENGTH_LONG).show();
-                setChartData(textUserName.getText().toString());
+                mImageView.setVisibility(View.GONE);
+                scan.setVisibility(View.GONE);
+                Toast.makeText(getApplicationContext(), "Scanning the Heart Beat", Toast.LENGTH_SHORT).show();
+                try {
+                    getData(textUserName.getText().toString());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                setChartData();
+                if (isBradycardia) {
+                    textViewDisplay.setVisibility(View.VISIBLE);
+                }
             }
         });
 
     }
 
-    void setChartData(String Name) {
+    private void showImage(String s) {
+//        mImageView.setImageBitmap(BitmapFactory.decodeFile(s+".png"));
+        try {
+            // get input stream
+            InputStream ims = getAssets().open(s + ".png");
+            // load image as Drawable
+            Drawable d = Drawable.createFromStream(ims, null);
+            // set image to ImageView
+            mImageView.setImageDrawable(d);
+            ims.close();
+            mImageView.setVisibility(View.VISIBLE);
+        } catch (IOException ex) {
+            return;
+        }
+        mImageView.setVisibility(View.VISIBLE);
+    }
+
+    void getData(String name) throws IOException {
+        String filePath = fileName.get(name);
+        demoData.clear();
+        LoadFile(filePath);
+    }
+
+    void setChartData() {
+        isBradycardia = Boolean.FALSE;
         chart.setVisibility(View.VISIBLE);
-        Log.d("Main_Activity", Name);
-        Float max = 100.f;
-        Float min = 20.0f;
         ArrayList<BarEntry> yVals = new ArrayList<>();
-        Random rand = new Random();
-        for (int i = 0; i < demoData.length; i++) {
-            yVals.add(new BarEntry(i, demoData[i]));
+        for (int i = 0; i < demoData.size(); i++) {
+            Float current = demoData.get(i);
+            if (current < 50.0f) {
+                isBradycardia = Boolean.TRUE;
+            }
+            yVals.add(new BarEntry(i, current));
+
         }
 
         MyBarDataSet set = new MyBarDataSet(yVals, "");
@@ -82,4 +135,24 @@ public class MainActivity extends AppCompatActivity {
         BarData data = new BarData(set);
         chart.setData(data);
     }
+
+    public void LoadFile(String fileName) throws IOException {
+        //Create a InputStream to read the file int
+        //get the resource id from the file name
+
+//        int rID = resources.getIdentifier("com.assignment.mc.heatbeat:raw/" + fileName, null, null);
+        //get the file as a stream
+        Log.d("MainActivity", fileName);
+//        AssetFileDescriptor descriptor = getAssets().openFd(fileName);
+//        FileReader reader = new FileReader(descriptor.getFileDescriptor());
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(context.getAssets().open(fileName + ".txt")));
+//        reader = new BufferedReader(new InputStreamReader(Freader));
+        String line = reader.readLine();
+        while (line != null) {
+            demoData.add(Float.valueOf(line));
+            line = reader.readLine();
+        }
+    }
+    //return the output stream as a String
 }
